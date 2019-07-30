0008e37b9e8598344fe25e2aeecbbdbe68bfa1d9
Jacob Werderits <werderitsj@gmail.com>
2019-07-29 23:22:41 -0400

    add opportunities endpoint

--- tap_copper/streams/__init__.py
+++ tap_copper/streams/__init__.py
@@ -2,18 +2,21 @@ from tap_copper.streams.users import UsersStream
 from tap_copper.streams.people import PeopleStream
 from tap_copper.streams.leads import LeadsStream
 from tap_copper.streams.companies import CompaniesStream
+from tap_copper.streams.opportunities import OpportunitiesStream
 
 
 AVAILABLE_STREAMS = [
-    UsersStream,
-    PeopleStream,
-    LeadsStream,
-    CompaniesStream,
+   UsersStream,
+   PeopleStream,
+   LeadsStream,
+   CompaniesStream,
+    OpportunitiesStream,
 ]
 
 __all__ = [
-    'UsersStream',
-    'PeopleStream',
-    'LeadsStream',
-    'CompaniesStream',
+   'UsersStream',
+   'PeopleStream',
+   'LeadsStream',
+   'CompaniesStream',
+    'OpportunitiesStream',
 ]
