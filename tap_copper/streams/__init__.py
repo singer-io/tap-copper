06ef2f3b91f6e57530e00d41d44c4fb97f8255c8
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-09-25 15:15:11 +0530

    Fix: Removed the pipeline stages

--- tap_copper/streams/__init__.py
+++ tap_copper/streams/__init__.py
@@ -9,7 +9,6 @@ from tap_copper.streams.leads import Leads
 from tap_copper.streams.loss_reasons import LossReasons
 from tap_copper.streams.opportunities import Opportunities
 from tap_copper.streams.people import People
-from tap_copper.streams.pipeline_stages import PipelineStages
 from tap_copper.streams.pipelines import Pipelines
 from tap_copper.streams.projects import Projects
 from tap_copper.streams.tags import Tags
@@ -28,7 +27,6 @@ STREAMS = {
     "loss_reasons": LossReasons,
     "opportunities": Opportunities,
     "people": People,
-    "pipeline_stages": PipelineStages,
     "pipelines": Pipelines,
     "projects": Projects,
     "tags": Tags,
