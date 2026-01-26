cf2c43d94637aab22d8d29632f2045436ad6066e
Dylan Sprayberry <dylan.sprayberry@qlik.com>
2026-01-26 19:19:56 +0000

    Version bump and changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="tap-copper",
-      version="0.1.0",
+      version="0.1.1",
       description="Singer.io tap for extracting data from copper API",
       author="Stitch",
       url="http://singer.io",
